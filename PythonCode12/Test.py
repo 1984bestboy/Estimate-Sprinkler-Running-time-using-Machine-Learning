3a53097cbaef6ac24990397d99df262f7e13aba1
ssgandham <gandhamsnehalsrinivas@gmail.com>
2017-05-29 14:31:40 -0700

    The excel file is getting updated from both ends

--- PythonCode12/Test.py
+++ PythonCode12/Test.py
@@ -1,5 +1,6 @@
 
 # Reference - https://stackoverflow.com/questions/26946337/reading-serial-data-from-arduino-project-pyserial
+# Reference - https://stackoverflow.com/questions/13381384/modify-an-existing-excel-file-using-openpyxl-in-python?rq=1
 
 import serial
 import sys
@@ -28,18 +29,35 @@ else:
     print "# using hard coded defaults " + port + " " + str(baudrate)
     ser = serial.Serial(port, baudrate)
 
+# New Code
+from openpyxl import load_workbook
+import csv
 
+#Open an xlsx for reading
+wb = load_workbook("/Users/balaji/Documents/Github/IOT/Weather/src/newFile.xlsx")
+#Get the current Active Sheet
+ws = wb.get_active_sheet()
+#You can also select a particular sheet
+#based on sheet name
+ws = wb.get_sheet_by_name("Current Weather")
 
+#ws.cell(row=1,column=12).value = 123456789
+#ws.cell(row=1,column=13).value = 123456789
+#ws.cell(row=1, column=14).value = 123456789
+#save the csb file
+wb.save("/Users/balaji/Documents/Github/IOT/Weather/src/newFile.xlsx")
+# New Code
+last_row=ws.max_row
 l = -1
-rb = open_workbook("/Users/balaji/Documents/Github/IOT/Weather/src/package_iot/newFile.xlsx")
-wb = copy(rb)
-s = wb.get_sheet(0)
-s.write(0, 0, "Humidity")
-s.write(0, 1, "Temperature")
-s.write(0, 2, "Moisture")
+# rb = open_workbook("/Users/balaji/Documents/Github/IOT/Weather/src/package_iot/newFile.xlsx")
+# wb = copy(rb)
+# s = wb.get_sheet(0)
+# s.write(0, 0, "Humidity")
+# s.write(0, 1, "Temperature")
+# s.write(0, 2, "Moisture")
 while i < 10:
     print "Current Value : " + ser.readline()
-
+    i=i+1
     string_line = ser.readline()
     list_values = string_line.split(',')
 
@@ -61,11 +79,12 @@ while i < 10:
             print "Moisture : " + item
             total_mositure_level = total_mositure_level + float(item)
 
-s.write(1, 0, total_humidity / 10)
-s.write(1, 1, total_temperature / 10)
-s.write(1, 2, total_temperature / 10)
+ws.cell(row=last_row,column=13).value = (total_humidity / 10)
+ws.cell(row=last_row,column=14).value = (total_temperature / 10)
+ws.cell(row=last_row,column=15).value = (total_mositure_level / 10)
 
+wb.save("/Users/balaji/Documents/Github/IOT/Weather/src/newFile.xlsx")
 # sys.stdout.write(string_line)
 
-wb.save('/Users/balaji/Desktop/Workbook2.xls')
+#wb.save('/Users/balaji/Desktop/Workbook2.xls')
 # sys.stdout.flush()
